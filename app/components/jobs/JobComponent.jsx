32bee42d6e93e65637ec182603bf6305cadff1dc
Damian Jankov <jankovdamian@gmail.com>
2026-02-22 16:17:05 +0100

    Not needed

--- app/components/jobs/JobComponent.jsx
+++ app/components/jobs/JobComponent.jsx
@@ -4,7 +4,6 @@ import
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
-import './jobs.css';
 
 export default function JobComponent({ imgPath, jobTitle, companyName, jobDescription, jobDate })
 {
